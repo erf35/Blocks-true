52d466e94ba717e024fa8462a08061739f53e842
erf35 <106652034+erf35@users.noreply.github.com>
2022-06-01 17:57:24 +0200

    added led control

--- led.control.ts
+++ led.control.ts
@@ -1,1 +1,16 @@
-// Add your code here
+//% weight=999 color=#ffb7c5 icon=""
+namespace ledfortime {
+    /**
+     * ledfortime
+     * @param n led x position, eg: 0
+     * @param s led y position, eg: 0
+    *  @param g how long led will be turned on, eg: 0
+     */
+    //% block="led x $n led y $s time ms $g"
+    export function ledtime(n: number, s: number, g: number): void {
+        // Add code here
+        led.plot(n, s)
+        basic.pause(g)
+        basic.clearScreen()
+    }
+}
